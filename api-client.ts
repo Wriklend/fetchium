5439600a070483586cff1dd09d1fce6bd2cbc424
Alexey Mushegov <Alexey.Mushegov@mediainstinctgroup.ru>
2022-02-09 02:42:52 +0300

    feat: class Fetchium and tsconfig

--- api-client.ts
+++ api-client.ts
@@ -31,6 +31,14 @@ const getExtractedRequestInit = (options: any): RequestInit =>
     return requestInitObject;
   }, {});
 
+const getResponseData = (body: any) => {
+  if (body instanceof FormData) {
+    return body;
+  }
+
+  return JSON.stringify(body);
+};
+
 export enum HttpMethod {
   get = "GET",
   post = "POST"
@@ -79,11 +87,11 @@ export default function compose<T>(...funcs: any[]): T {
 class Fetchium {
   defaultOptions: DefaultOptions = {};
 
-  private requestInterposers: InterposeFunction<
-    FetchiumRequestOptions,
-    FetchiumRequestOptions | null
-    > = () => null;
+  private requestInterposers: InterposeFunction<FetchiumRequestOptions, FetchiumRequestOptions | null> = () => null;
+
   private responseInterposers: InterposeFunction<any, any> = () => {};
+
+  private errorInterposers: any = () => {};
 
   constructor(baseUrl?: string, headers: Headers = defaultHeaders) {
     this.defaultOptions.baseUrl = baseUrl ?? null;
@@ -91,10 +99,8 @@ class Fetchium {
   }
 
   public composeRequestInterposers(
-    ...reqInt: InterposeFunction<
-      FetchiumRequestOptions,
-      FetchiumRequestOptions
-      >[]
+    ...reqInt: InterposeFunction<FetchiumRequestOptions,
+      FetchiumRequestOptions>[]
   ) {
     this.requestInterposers = compose(...reqInt);
     return this;
@@ -107,11 +113,17 @@ class Fetchium {
     return this;
   }
 
+  public composeErrorInterposers(
+    ...responseErrorFuncs: any[]
+  ) {
+    this.errorInterposers = compose(...responseErrorFuncs);
+    return this;
+  }
+
   private buildUrl(path: string, params: any): string {
-    const url =
-      isAbsoluteURL(path) || !this.defaultOptions.baseUrl
-        ? path
-        : combineUrls(this.defaultOptions.baseUrl, path);
+    const url = isAbsoluteURL(path) || !this.defaultOptions.baseUrl
+      ? path
+      : combineUrls(this.defaultOptions.baseUrl, path);
 
     if (!params) {
       return url;
@@ -123,48 +135,74 @@ class Fetchium {
     return fullUrl.toString();
   }
 
+  private buildHeaders = (headers: HeadersInit): HeadersInit => {
+    const requestHeaders: HeadersInit = new Headers();
+    Object.entries(headers).forEach(([key, value]) => {
+      requestHeaders.set(key, value);
+    });
+
+    return requestHeaders;
+  };
+
   private async request<T>(
     url: string,
     method: HttpMethod,
-    options?: FetchiumRequestOptions
-  ): Promise<T | void> {
+    options?: FetchiumRequestOptions,
+  ): Promise<T> {
     try {
-      const config = this.requestInterposers(options);
+      const config = this.requestInterposers({ ...this.defaultOptions, ...options });
       const completeUrl = this.buildUrl(url, config?.params);
 
+      const headers = this.buildHeaders(config.headers);
+
       const request = new Request(completeUrl, {
-        method: method,
-        headers: config?.headers ?? this.defaultOptions.headers,
-        ...(config?.data ? { body: JSON.stringify(config.data) } : null),
-        ...(config ? getExtractedRequestInit(config) : null)
+        method,
+        headers,
+        ...(config?.data ? { body: getResponseData(config.data) } : null),
+        ...(config ? getExtractedRequestInit(config) : null),
       });
 
       const response = await fetch(request);
 
+      // This really important disable, cause we don`t must await response func
+      // eslint-disable-next-line @typescript-eslint/return-await
       return this.response<T>(response);
     } catch (e) {
-      console.log(e);
+      console.log('request Error', e);
+      return e;
     }
   }
 
-  private async response<T>(response: Response): Promise<T | void> {
+  private async response<T>(response: Response): Promise<T> {
     try {
       if (!response.ok) {
-        throw new Error(response.statusText);
+        this.errorInterposers(response);
+
+        // ¯\_(ツ)_/¯
+        // eslint-disable-next-line @typescript-eslint/return-await
+        return new Promise((resolve, reject) => {
+          reject(response);
+        });
       }
 
       const data = await response.json();
 
       return this.responseInterposers(data);
     } catch (e) {
-      console.log(e);
+      console.log('response Error: ', e);
+      return (e);
     }
   }
 
   async get<T>(
     url: string,
-    options?: FetchiumRequestOptions
+    options?: FetchiumRequestOptions,
   ): Promise<T | void> {
     return this.request<T>(url, HttpMethod.get, options);
   }
+
+  post<T>(url, options?: FetchiumRequestOptions): Promise<T> {
+    return this.request(url, HttpMethod.post, options);
+  }
 }
+
